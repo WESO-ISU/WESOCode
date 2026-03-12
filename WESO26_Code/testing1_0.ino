5d11d13b219860fad9084d01247c1e837e264879
Elijah Herring <eliherrr@iastate.edu>
2026-03-12 03:06:57 -0500

    added basic linear actuator methods for testing

--- WESO26_Code/testing1_0.ino
+++ WESO26_Code/testing1_0.ino
@@ -1,4 +1,8 @@
 //NEED TO KNOW SPECIFIC LIBRARIES TO USE FOR EACH COMPONENT.
+#include <servo.h>
+#include <SD.h>
+
+
 int LOAD_PIN1 = 2;
 int LOAD_PIN2 = 3;
 int LOAD_PIN3 = 4;
@@ -19,9 +23,29 @@ int LA_PIN = 0;
 int ESTOP_WRITE_PIN = 22;
 int ESTOP_READ_PIN = 23;
 
+int LA_EXTEND_BTN_PIN = 24;
+int LA_RETRACT_BTN_PIN = 25;
+
+const int LA_EXTEND = 120; //This should be the furthest out the linear actuator can move. Adjust as needed. 
+const int LA_RETRACT = 80; //This should be the furthest in the linear actuator can move. Adjust as needed.
+//All linear actuator movement needs to be bounded by these numbers.
+const int LA_STEP_SIZE = 1; //Common step size for moving linear actuator. 
+const int LA_HOME_POSITION = 102; //This is the home position of the linear actuator, here.
+int la_current_position = 102; //This also acts as the starting position for la. Remeber to change this value whenever the linear actuator is moved.
+
+
+Servo linearActuator;
+
+
 void setup() {
   pinMode(IR_PIN, INPUT);
   Serial.begin(9600);
+
+
+  linearActuator.attach(LA_PIN);
+  linearActuator.write((LA_HOME_POSITION));
+  pinMode(LA_EXTEND_BTN_PIN, INPUT_PULLUP);
+  pinMode(LA_RETRACT_BTN_PIN, INPUT_PULLUP);
 }
 
 
@@ -55,6 +79,30 @@ void set_pitch_angle(float angle); //Requires analogWrite()
 
 //Data collection methods 
 
+//Linear actuator methods.
+void handle_actuator_buttons(){
+    bool extendPressed = digitalRead(LA_EXTEND_BTN_PIN) == LOW;
+    bool retractPressed = digitalRead(LA_RETRACT_BTN_PIN) == LOW;
+
+    if(extendPressed && is_legal_la_step(LA_STEP_SIZE)){
+        la_current_position += LA_STEP_SIZE;
+        linearActuator.write(la_current_position);
+        delay(15); //Allows actuator to catch up
+    } else if(retractPressed && is_legal_la_step(LA_STEP_SIZE)){
+        la_current_position -= LA_STEP_SIZE;
+        linearActuator.write(la_current_position);
+        delay(15);
+    }
+}
+
+bool is_legal_la_step(int step_size){
+    return !(la_current_position + step_size > LA_EXTEND || la_current_position - step_size < LA_RETRACT);
+}
+
+bool is_legal_la_write(int write_value){
+    return !(write_value > LA_EXTEND || write_value < LA_RETRACT);
+}
+
   
 
 void record_rpm(); // write to sd card  
@@ -81,7 +129,7 @@ int lastState = HIGH;
 unsigned long pulseTimes[5];
 int pulseIndex = 0;
 bool bufferFull = false;
-int NUM_BLADES = 5;
+int NUM_BLADES = 1;
 
 float get_rpm() {
   int sensorVal = digitalRead(IR_PIN);
